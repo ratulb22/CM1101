9d9226abc0543123356a31e567bd2ac464fee048
Ratul Banerjee <ratulb22@gmail.com>
2018-10-19 13:15:49 +0100

    Final Work

---
+++ Game 2/player.py
@@ -0,0 +1,9 @@
+from items import *
+from map import rooms
+
+inventory = [item_id, item_laptop, item_money]
+mass = items['laptop']['mass'] + items['id']['mass'] + items['money']['mass']
+max_weight = 7
+
+# Start game at the reception
+current_room = rooms["Reception"]
